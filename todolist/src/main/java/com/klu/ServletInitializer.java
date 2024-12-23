3d3dd9cd9022dcf6c8b1092e16713582a728a9a0
SachidanandaTripathy <sachidananda.tripathy03@gmail.com>
2024-12-23 21:33:13 +0530

    frontend added

--- todolist/src/main/java/com/klu/ServletInitializer.java
+++ todolist/src/main/java/com/klu/ServletInitializer.java
@@ -4,10 +4,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// Links the application class
 		return application.sources(TodolistApplication.class);
 	}
-
 }
--- todolist/src/main/java/com/klu/TodolistApplication.java
+++ todolist/src/main/java/com/klu/TodolistApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodolistApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-	}
 
+	}
 }
